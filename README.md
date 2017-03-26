e0f196bf8aa93946bbb255525a81ee80dd61d564
Jeffery <jeffery@influx.io>
2017-03-26 15:29:43 +0800

    open source javascipr type check utils

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # qatype
-Simple JavaScript type checking module. Returns a Boolean or type name for each type check.
+A Simple JavaScript type checking module for JavaScript that returns a `Boolean` or `type name` for each type check.
+
+```js
+	qatype.isArray([1,2]); // true
+	qatype.isObject({a:1}); // true
+	qatype.isString('test string'); // true
+	qatype.isDate(new Date()); // true
+	qatype.isRegExp(/^qatype/i); // true
+	qatype.isFunction(function () {}); // true
+	qatype.isBoolean(true); // true
+	qatype.isNumber(8); // true
+	qatype.isNull(null); // true
+	qatype.isUndefined(); // true
+
+	// Usage the similar with above code
+	qatype.type({}); //Object,It will return type name for object
+```
+
+### Manual installation
+Ensure you're using the files from the dist directory (contains compiled production-ready code). Ensure you place the script before the closing `</body>` tag.
+```html
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	    <title>qatype.js</title>
+	</head>
+	<body>
+	  <script type="text/javascript" src="dist/qatype.min.js"></script>
+	  <script type="text/javascript" >
+	  // qatype.js module available
+	  </script>
+	</body>
+</html>
+```
---
+++ dist/qatype.js
@@ -0,0 +1,64 @@
+/**
+ * qatype.js
+ * Author:ty4z2008
+ * Simple JavaScript type checking module.
+ */
+
+(function(root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof exports === 'object') {
+        module.exports = factory();
+    } else {
+        root.qatype = factory();
+    }
+}(this, function() {
+    /**
+     * plugin name
+     */
+    var qatype = {};
+
+    //base type
+    var _types = ['Array', 'Object', 'String', 'Number', 'Date', 'RegExp', 'Function', 'Boolean', 'Null', 'Undefined'];
+
+    /**
+     * Detect Object type
+     * @return {String} Array,Object...
+     */
+    function _type() {
+        return Object.prototype.toString.call(this).slice(8, -1);
+    }
+    /**
+     * Use qatype.isFunction(obj) you can detect this type for object
+     * qatype.isArray([]); // true
+     * qatype.isObject({}); // true
+     * qatype.isString(''); // true
+     * qatype.isDate(new Date()); // true
+     * qatype.isRegExp(/test/i); // true
+     * qatype.isFunction(function () {}); // true
+     * qatype.isBoolean(true); // true
+     * qatype.isNumber(1); // true
+     * qatype.isNull(null); // true
+     * qatype.isUndefined(); // true
+     * @param {Mixed} Object
+     * @return {Boolean} true/false
+     */
+    for (var i = _types.length; i--;) {
+        var type = _types[i];
+        qatype['is' + type] = (function(self) {
+            return function(obj) {
+                return _type.call(obj) === self;
+            };
+        })(type);
+    }
+    /**
+     * Detect type for Object. It will return type name
+     * @param {Mixed} Object
+     * @return {String} ...
+     */
+    qatype['type'] = function(obj) {
+        return _type.call(obj);
+    }
+
+    return qatype;
+}));
